1ac46fbc674e0e2863bbae4bdb387ee5b57d44ee
Andy Gutsche <andy.gutsche@aperto.com>
2017-07-26 09:55:20 +0200

    Add yarn lock

--- resources/js/templates/templates.js
+++ resources/js/templates/templates.js
@@ -8,7 +8,7 @@ window["App"]["Templates"]["OVERLAY"] = Handlebars.template({"compiler":[7,">= 4
 },"useData":true});
 
 window["App"]["Templates"]["TYPEAHEAD__ITEM"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<a href=\"#"
     + alias4(((helper = (helper = helpers.keyword || (depth0 != null ? depth0.keyword : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"keyword","hash":{},"data":data}) : helper)))
@@ -20,16 +20,16 @@ window["App"]["Templates"]["TYPEAHEAD__ITEM"] = Handlebars.template({"compiler":
 window["App"]["Templates"]["TYPEAHEAD"] = Handlebars.template({"1":function(container,depth0,helpers,partials,data) {
     var helper;
 
-  return container.escapeExpression(((helper = (helper = helpers.typeAheadContextClass || (depth0 != null ? depth0.typeAheadContextClass : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"typeAheadContextClass","hash":{},"data":data}) : helper)));
+  return container.escapeExpression(((helper = (helper = helpers.typeAheadContextClass || (depth0 != null ? depth0.typeAheadContextClass : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"typeAheadContextClass","hash":{},"data":data}) : helper)));
 },"3":function(container,depth0,helpers,partials,data) {
     return "default";
 },"5":function(container,depth0,helpers,partials,data) {
     var helper;
 
   return " "
-    + container.escapeExpression(((helper = (helper = helpers.typeAheadClass || (depth0 != null ? depth0.typeAheadClass : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"typeAheadClass","hash":{},"data":data}) : helper)));
+    + container.escapeExpression(((helper = (helper = helpers.typeAheadClass || (depth0 != null ? depth0.typeAheadClass : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"typeAheadClass","hash":{},"data":data}) : helper)));
 },"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, alias1=depth0 != null ? depth0 : {};
+    var stack1, alias1=depth0 != null ? depth0 : (container.nullContext || {});
 
   return "<div class=\"c-type-ahead--"
     + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.typeAheadContextClass : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.program(3, data, 0),"data":data})) != null ? stack1 : "")
