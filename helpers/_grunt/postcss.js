747e56d9f2b783896a98fc67f411f1faf05b7af9
Andy Gutsche <andy.gutsche@aperto.de>
2017-07-28 10:56:56 +0200

    Make type-ahead compatible to veams-query + fix deprecated warnings in bourbon + add autoprefixer

--- helpers/_grunt/postcss.js
+++ helpers/_grunt/postcss.js
@@ -1,12 +1,12 @@
 module.exports = {
 	options: {
-    log: true,
-    processors: [
-		require('autoprefixer')({
-			browsers: ['last 2 versions', 'ie >= 10']
-     	})
-    ]
-  },
+		log: true,
+		processors: [
+			require('autoprefixer')({
+				browsers: ['last 2 versions', 'ie >= 10', 'iOS >= 10', 'Android >= 6']
+			})
+		]
+	},
 	dev: {
 		options: {
 			map: true
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 // Global dependencies 
-import $ from "jquery";
+import $ from "./vendor/veams-query/src/veams-query";
 import 'lazysizes';
 import Handlebars from 'handlebars/runtime';
 import VeamsCore from 'veams/src/js/generics/core';
