476222ad9640e97670fb6d15b7ca58621a652fee
Andy Gutsche <andy.gutsche@aperto.de>
2017-07-28 10:59:57 +0200

    Use jquery

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,5 @@
 // Global dependencies 
-import $ from "./vendor/veams-query/src/veams-query";
+import $ from "jquery";
 import 'lazysizes';
 import Handlebars from 'handlebars/runtime';
 import VeamsCore from 'veams/src/js/generics/core';
